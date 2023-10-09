cbb30e32249581eed1117738db72fd3cc897ef31
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-10-09 20:38:51 +0530

    server connected

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,12 @@
 import { app } from "./app";
+require('dotenv').config()
+
 
 
 
 
 // create our server
+app.listen(process.env.PORT,()=>{
+    console.log(`server is connected with ${process.env.PORT}`);
+    
+} )
