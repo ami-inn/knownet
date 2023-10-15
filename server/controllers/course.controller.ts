90a693608be750bf1f523be0199bd7121d2a3f8a
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-10-15 19:23:57 +0530

    get single and all coursed completed  - server

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -71,3 +71,38 @@ export const editCourse = CatchAsynError(async(req:Request,res:Response,next:Nex
         return next(new ErrorHandler(error.message,500))
     }
 })
+
+// get single course = we dont purchasing every one to acess this 
+
+export const getSingleCourse =  CatchAsynError(async(req:Request,res:Response,next:NextFunction) =>{
+    try {
+
+        const course = await courseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        res.status(200).json({
+            success:true,
+            course
+        })
+        
+    } catch (error : any) {
+      return next(new ErrorHandler(error.message,400))        
+    }
+})
+
+// get all the courses without purchase
+
+export const getAllCourses = CatchAsynError(async(req:Request,res:Response,next:NextFunction) => {
+
+    try {
+
+        const courses = await courseModel.find().select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links")
+
+        res.status(200).json({
+            success:true,
+            courses
+        })
+        
+    } catch (error:any) {
+        return next(new ErrorHandler(error.message,400))
+    }
+})
