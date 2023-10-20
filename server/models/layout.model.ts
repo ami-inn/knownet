3aac2e92f9d686759a8d0bab507d5f64e044794d
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-10-20 15:58:19 +0530

    layout model created - server

---
+++ server/models/layout.model.ts
@@ -0,0 +1,57 @@
+import { Schema, Document,Model, model } from "mongoose";
+
+
+interface faqItem extends Document{
+    question: string;
+    answer: string;
+
+}
+
+interface category extends Document {
+    title: string;
+}
+
+interface bannerImage extends Document{
+    public_id: string;
+    url:string
+}
+
+interface Layout extends Document {
+    type: string;
+    faq:faqItem[];
+    category:category[];
+    banner:{
+        image:bannerImage;
+        title:string;
+        subtitle:string;
+    }
+}
+
+const faqSchema = new Schema<faqItem>({
+    question:{type:String},
+    answer:{type:String}
+})
+
+const categorySchema = new Schema<category>({
+    title:String
+})
+
+const bannerImageSchema = new Schema<bannerImage>({
+    public_id:{type:String},
+    url:{type:String}
+})
+
+const layoutSchema = new Schema<Layout>({
+    type:{type:String},
+    faq:[faqSchema],
+    category:[categorySchema],
+    banner:{
+        image:bannerImageSchema,
+        title:{type:String},
+        subtitle:{type:String}
+    }
+})
+
+const layoutModel = model<Layout>('Layout',layoutSchema)
+
+export default layoutModel
