7f6aec0e37b723969b992a8fa726d0c65e6d2d8f
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-10-22 17:40:08 +0530

    utils heading and theme provider added - client

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,8 @@
 
-'use client'
+'use client' // because this client component
 
 import React,{FC,useState} from "react"
+import Heading from "./utils/Heading"
 
 interface Props {}
 
@@ -9,7 +10,11 @@ interface Props {}
 const Page:FC<Props> = (props) =>{
   return(
     <div>
-      
+      <Heading 
+      title="Know net"
+      description="Know net is a platform for students to learn and get help from teachers"
+      keywords="programming, mern, Ai" 
+      />
     </div>
   )
 }
