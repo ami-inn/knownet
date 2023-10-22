abac85a37a28735e316d7bc05ace273f8b47c15b
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-10-22 21:00:14 +0530

    header created and add some styles - client

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,17 +3,27 @@
 
 import React,{FC,useState} from "react"
 import Heading from "./utils/Heading"
+import Header from "./components/Header"
 
 interface Props {}
 
 
 const Page:FC<Props> = (props) =>{
+
+  const [open,setOpen] = useState(false)
+  const [activeItem,setActiveItem] = useState(0)
+
   return(
     <div>
       <Heading 
       title="Know net"
       description="Know net is a platform for students to learn and get help from teachers"
       keywords="programming, mern, Ai" 
+      />
+      <Header
+      open={open}
+      setOpen={setOpen}
+      activeItem={activeItem}
       />
     </div>
   )
