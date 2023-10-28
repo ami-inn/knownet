84085cf0afdc07bb242796df9564c931bf30450e
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-10-28 10:38:16 +0530

    redux authApi activation mutation  added - client

--- client/redux/features/auth/authApi.ts
+++ client/redux/features/auth/authApi.ts
@@ -1,44 +1,53 @@
 import { apiSlice } from "../api/apiSlice";
 import { userRegistration } from "./authSlice";
 
-
 type RegistrationResponse = {
-    message: string
-    activationToken: string
-}
+  message: string;
+  activationToken: string;
+};
 
-type RegistrationData = {}
-
+type RegistrationData = {};
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints:(builder)=>({
-        // all end poinst will be here
-        // in rtk query only two type req query = get or fetch mutation put post like that
-        register:builder.mutation<RegistrationResponse,RegistrationData>({
-            query:(data)=>({
-                url:"registration", // localhost:8000/api/v1/registration this mean it
-                method:"post",
-                body:data,
-                credentials:"include" as const, // afrer getting data we need to store it in our reducer 
-            }),
+  endpoints: (builder) => ({
+    // all end poinst will be here
+    // in rtk query only two type req query = get or fetch mutation put post like that
+    register: builder.mutation<RegistrationResponse, RegistrationData>({
+      query: (data) => ({
+        url: "registration", // localhost:8000/api/v1/registration this mean it
+        method: "post",
+        body: data,
+        credentials: "include" as const, // afrer getting data we need to store it in our reducer
+      }),
 
-            async onQueryStarted(arg,{queryFulfilled,dispatch}){
-                try {
-                    const result = await queryFulfilled // we get the data when it success
-                    dispatch(
-                        userRegistration({
-                            token:result.data.activationToken
-                        })
-                    )
-                    
-                } catch (error:any) {
-                    console.log(error);
-                    
-                }
-            }
-        })
-    })
-})
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled; // we get the data when it success
+          dispatch(
+            userRegistration({
+              token: result.data.activationToken,
+            })
+          );
+        } catch (error: any) {
+          console.log(error);
+        }
+      },
+    }),
+
+    activation: builder.mutation({
+      query: ({ activation_token, activation_code }) => ({
+        // got to server activation token and code from req.body check it on backerns  // we recieving this arguments values
+        url: "activate-user",
+        method: "POST",
+        body: {
+          activation_token,
+          activation_code,
+        },
+      }),
+    }),
+
+  }),
+});
 // this action is ready we can call it
 
-export const {useRegisterMutation} = authApi
+export const { useRegisterMutation,useActivationMutation } = authApi;
