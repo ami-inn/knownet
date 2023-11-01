10a58c8a542b2c569f629d11cc67c066664b2549
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-11-01 19:47:16 +0530

    just layout created for the sider bar of profile

--- client/app/components/Profle/SiderBarProfile.tsx
+++ client/app/components/Profle/SiderBarProfile.tsx
@@ -1,17 +1,38 @@
-import React,{FC} from 'react'
+import Image from "next/image";
+import React, { FC } from "react";
+import avatarDefault from "../../../public/assets/avatar.jpg";
 
 type Props = {
-    user:any,
-    active:number,
-    avatar:string|null,
-    setActive:(active:number) => void
-    logoutHandler:any
-}
+  user: any;
+  active: number;
+  avatar: string | null;
+  setActive: (active: number) => void;
+  logoutHandler: any;
+};
 
-const SiderBarProfile:FC<Props> = ({user,active,avatar,setActive,logoutHandler}) => {
+const SiderBarProfile: FC<Props> = ({
+  user,
+  active,
+  avatar,
+  setActive,
+  logoutHandler,
+}) => {
   return (
-    <div>SiderBarProfile</div>
-  )
-}
+    <div className="w-full">
+      <div
+        className={`w-full flex items-center px-3 py-4 cursor-pointer ${
+          active === 1 ? "bg-slate-800" : "bg-transparent"
+        }`}
+        onClick={() => setActive(1)}
+      >
+        <Image
+          src={user.avatar || avatar ? user.avatar : avatarDefault}
+          alt=""
+          className="w-[20px] h-[20px] 800px:w-[30px] 800px:h-[30px] cursor-pointer rounded-full"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default SiderBarProfile
+export default SiderBarProfile;
